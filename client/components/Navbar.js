4173a272b6b1a14e7c5f6a74332fde9871092e7c
Freddy Gomez <fjohngomez90@gmail.com>
2022-01-25 17:48:57 -0500

    Updated Navbar component to functional along with MUI styles pending ux update

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,47 +1,50 @@
 import React from 'react'
-import {connect} from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
+import { Box, AppBar, Toolbar, IconButton, Typography, Button } from '@mui/material'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
-    <h1>FS-App-Template</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-    </nav>
-    <hr />
-  </div>
-)
+function Navbar () {
+  const isLoggedIn = useSelector(state => !!state.auth.id);
+  const dispatch = useDispatch()
 
-/**
- * CONTAINER
- */
-const mapState = state => {
-  return {
-    isLoggedIn: !!state.auth.id
+  const handleClick = () => {
+    dispatch(logout())
   }
+
+
+  return (
+    <Box sx={{ flexGrow: 1}}>
+      <AppBar position="static">
+
+        <Toolbar variant="dense">
+          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2}}>
+            Menu
+          </IconButton>
+
+          {isLoggedIn ? (
+              <>
+            <Link to="/home">
+            <Typography variant="h6" color="inherit" component="div" sx={{ textDecoration: "none"}}>
+              Home
+            </Typography>
+            </Link>
+            <Button onClick={handleClick} sx={{color: "white"}}>
+              Logout
+            </Button>
+            </>
+            ) : (
+              <>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Sign Up</Link>
+              </>
+          )}
+
+
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
 
-const mapDispatch = dispatch => {
-  return {
-    handleClick() {
-      dispatch(logout())
-    }
-  }
-}
-
-export default connect(mapState, mapDispatch)(Navbar)
+export default Navbar
