177903bf6145109bd5e44124776927dffeaa2178
Freddy Gomez <fjohngomez90@gmail.com>
2022-01-26 16:51:39 -0500

    Package Install: Reduxjs/toolkit, reconfigured BP code for store index

--- client/store/index.js
+++ client/store/index.js
@@ -1,14 +1,23 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 import {createLogger} from 'redux-logger'
-import thunkMiddleware from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
 import auth from './auth'
 
-const reducer = combineReducers({ auth })
-const middleware = composeWithDevTools(
-  applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
-)
-const store = createStore(reducer, middleware)
+import {createStore, combineReducers, applyMiddleware} from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
+
+// const reducer = combineReducers({ auth })
+// const middleware = composeWithDevTools(
+//   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
+// )
+// const store = createStore(reducer, middleware)
+
+const store = configureStore({
+  reducer: {
+    auth,
+  },
+  middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), createLogger({collapsed: true})]
+})
 
 export default store
 export * from './auth'
