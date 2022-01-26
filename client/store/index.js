dd579c9471095fde32a43c752b50d9ee6e9eaf12
Freddy Gomez <fjohngomez90@gmail.com>
2022-01-26 16:55:48 -0500

    code cleanup

--- client/store/index.js
+++ client/store/index.js
@@ -2,22 +2,11 @@ import { configureStore } from '@reduxjs/toolkit'
 import {createLogger} from 'redux-logger'
 import auth from './auth'
 
-import {createStore, combineReducers, applyMiddleware} from 'redux'
-import thunkMiddleware from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
-
-// const reducer = combineReducers({ auth })
-// const middleware = composeWithDevTools(
-//   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
-// )
-// const store = createStore(reducer, middleware)
-
 const store = configureStore({
   reducer: {
     auth,
   },
   middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), createLogger({collapsed: true})]
 })
-
 export default store
 export * from './auth'
