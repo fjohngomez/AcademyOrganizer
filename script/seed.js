9ada7f4f0500982167545a6db83199934c6f9508
Freddy Gomez <fjohngomez90@gmail.com>
2022-03-27 14:54:30 -0400

    rewrote seed code to split test data creation into arr for future changes using length of arrays

--- script/seed.js
+++ script/seed.js
@@ -6,60 +6,101 @@ const {db, models: {User, Student, Campus} } = require('../server/db')
  * seed - this function clears the database, updates tables to
  *      match the models, and populates the database.
  */
+
+const studentTestData = [
+  {
+    firstName: 'Freddy',
+    lastName: 'G',
+    email: 'me@me.com',
+    gpa: 3.4
+  },
+  {
+    firstName: 'Charlie',
+    lastName: 'Ackerman',
+    email: 'CAckerman@me.com',
+    gpa: 3.7
+  },
+  {
+    firstName: 'Steph',
+    lastName: 'Chane',
+    email: 'SChane@me.com',
+    gpa: 3.9
+  }
+]
+
+const campusTestData = [
+  {
+    name: 'City College',
+    address: '123 convent ave',
+    description: 'a school I went to'
+  },
+  {
+    name: 'York College',
+    address: '123 york ave',
+    description: 'Some School around'
+  },
+  {
+    name: 'Hostos Community College',
+    address: 'grand concourse ave',
+    description: 'a bronx community college'
+  }
+]
+
 async function seed() {
   await db.sync({ force: true }) // clears db and matches models to tables
   console.log('db synced!')
   // Students
-  const students = await Promise.all([
-    Student.create({
-      firstName: 'Freddy',
-      lastName: 'G',
-      email: 'me@me.com',
-      gpa: 3.4
-    }),
-    Student.create({
-      firstName: 'Charlie',
-      lastName: 'Ackerman',
-      email: 'CAckerman@me.com',
-      gpa: 3.7
-    }),
-    Student.create({
-      firstName: 'Steph',
-      lastName: 'Chane',
-      email: 'SChane@me.com',
-      gpa: 3.9
-    })
-  ])
+  // const students = await Promise.all([
+  //   Student.create({
+  //     firstName: 'Freddy',
+  //     lastName: 'G',
+  //     email: 'me@me.com',
+  //     gpa: 3.4
+  //   }),
+  //   Student.create({
+  //     firstName: 'Charlie',
+  //     lastName: 'Ackerman',
+  //     email: 'CAckerman@me.com',
+  //     gpa: 3.7
+  //   }),
+  //   Student.create({
+  //     firstName: 'Steph',
+  //     lastName: 'Chane',
+  //     email: 'SChane@me.com',
+  //     gpa: 3.9
+  //   }),
+  // ])
 
   // Campuses
-  const campuses = await Promise.all([
-    Campus.create({
-      name: 'City College',
-      address: '123 convent ave',
-      description: 'a school I went to'
-    }),
-    Campus.create({
-      name: 'York College',
-      address: '123 york ave',
-      description: 'Some School around'
-    }),
-    Campus.create({
-      name: 'Hostos Community College',
-      address: 'grand concourse ave',
-      description: 'a bronx community college'
-    })
-  ])
+  // const campuses = await Promise.all([
+  //   Campus.create({
+  //     name: 'City College',
+  //     address: '123 convent ave',
+  //     description: 'a school I went to'
+  //   }),
+  //   Campus.create({
+  //     name: 'York College',
+  //     address: '123 york ave',
+  //     description: 'Some School around'
+  //   }),
+  //   Campus.create({
+  //     name: 'Hostos Community College',
+  //     address: 'grand concourse ave',
+  //     description: 'a bronx community college'
+  //   })
+  // ])
 
   // Creating Users
   const users = await Promise.all([
     User.create({ username: 'cody', password: '123' }),
     User.create({ username: 'murphy', password: '123' }),
   ])
-  await students;
-  await campuses;
+  await Promise.all(campusTestData.map(campus => Campus.create(campus)))
+  const students = await Promise.all(studentTestData.map(student => Student.create(student))).then(values => console.log(values))
 
-  const student1 = await Student.findByPk(1)
-  await student1.setCampus(1)
+
+  // const student1 = await Student.findByPk(1)
+  // await student1.setCampus(1)
   console.log(`seeded ${users.length} users`)
   console.log(`seeded successfully`)
   return {
